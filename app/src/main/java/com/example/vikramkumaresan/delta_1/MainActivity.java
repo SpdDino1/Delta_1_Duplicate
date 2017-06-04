29cb9ea41b853b89b990b6203a5516f0305fb0da
Vikram Kumaresan <vikramkumaresan@rocketmail.com>
2017-06-04 11:41:45 +0300

    Just messing around with git

--- app/src/main/java/com/example/vikramkumaresan/delta_1/MainActivity.java
+++ app/src/main/java/com/example/vikramkumaresan/delta_1/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Just messing around
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
