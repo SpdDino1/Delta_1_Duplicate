8142d8fe14394913a9175cfedb41106de3ec6425
Vikram Kumaresan <vikramkumaresan@rocketmail.com>
2017-06-04 11:42:59 +0300

    .

--- app/src/main/java/com/example/vikramkumaresan/delta_1/MainActivity.java
+++ app/src/main/java/com/example/vikramkumaresan/delta_1/MainActivity.java
@@ -27,7 +27,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //Just messing around
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
